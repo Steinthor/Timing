d37b76edc34ec787128eb04f4635675e59a45ab2
Steinþór Jasonarson <steinthor@prismlabs.tech>
2024-04-24 11:43:29 +0200

    fixing shellcheck warnings

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@ set -e
 
 nthreads=$(nproc --all)
 
-if [ ! -d "build" ]; then
-  mkdir build
+if [[ ! -d "build" ]]; then
+	mkdir build
 fi
 cd build || exit
 cmake .. -DCMAKE_BUILD_TYPE="Release" -GNinja
-ninja -j "$nthreads"
+ninja -j "${nthreads}"
 sudo ninja install
 
 cd ..
